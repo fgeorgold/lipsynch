0845635f0993f84457ea4b88575ee29f0921dea7
Francesco Georg <fgeorg@Francescos-MacBook-Pro.local>
2012-05-23 13:51:56 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lipsynch::Application.config.secret_token = '0cec160358ff55c9cde27a170a769dc473cf2f62bc8da7afd077e3082125a7f8fe1a37c3a40ffbaf1c728b261088baf762eb326df168a0630dc2a29160a951f7'
